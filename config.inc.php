ae1c9c21546253f9e1078d5ff10ad77efece92c1
michal.schweichler <michal.schweichler@gmail.com>
2013-12-16 22:20:20 +0000

    second commint

---
+++ config.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+/* 
+ * @author Michal Schweichler <michal.schweichler@gmail.com>
+ * 
+ * @license http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public License
+ */
+
+// CONFIGURATION 
+
+
+
+// debug mode on/off
+$debug = true;
+if($debug)
+{
+    error_reporting(E_ALL);
+}
+else
+{
+    error_reporting(0);
+}
+
+
+
+
+
+
+
+
+
+?>
